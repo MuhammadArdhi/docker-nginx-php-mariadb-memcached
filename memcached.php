4353d34a274c3f4a4282bf9451e1b1bcc5505b89
MuhammadArdhi <xardhix@gmail.com>
2018-08-08 13:22:50 +0700

    memcached config for CodeIgniter

---
+++ memcached.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['memcached'] = [
+    'hostname' => 'memcached',
+    'port' => '11211',
+    'weight' => '1',
+];
+/* End of file memcached.php */
+/* Location: ./application/config/memcached.php */
